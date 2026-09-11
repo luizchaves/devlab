580e91130959bb877e7144896ac1e7ead4c888fc
Luiz Carlos <lucachaves@gmail.com>
2026-09-11 16:15:26 -0300

    docs: unify modeling diagrams under academic domain context and add full ER diagram

--- examples/courses/database/invest-db/modeling/school.sql
+++ examples/courses/database/invest-db/modeling/school.sql
@@ -8,6 +8,16 @@ CREATE TABLE Professor (
 );
 -- #endregion professor
 
+-- #region gabinete
+-- Entidade Gabinete (relacionamento 1:1 com Professor)
+CREATE TABLE Gabinete (
+  id           INTEGER PRIMARY KEY AUTOINCREMENT,
+  numero       TEXT    NOT NULL UNIQUE,
+  professor_id INTEGER NOT NULL UNIQUE,
+  FOREIGN KEY (professor_id) REFERENCES Professor(id)
+);
+-- #endregion gabinete
+
 -- #region turma
 -- Entidade Turma (lado N do relacionamento 1:N)
 CREATE TABLE Turma (
@@ -40,6 +50,7 @@ CREATE TABLE Turma_Aluno (
 
 -- #region check
 INSERT INTO Professor (nome) VALUES ('Marina');
+INSERT INTO Gabinete (numero, professor_id) VALUES ('B-102', 1);
 INSERT INTO Turma (codigo, professor_id) VALUES ('BD-2026', 1);
 INSERT INTO Aluno (nome) VALUES ('João'), ('Lia');
 INSERT INTO Turma_Aluno VALUES (1, 1, '2026-02-10'), (1, 2, '2026-02-11');
